4794f980b10405e4b060b7497ceaf6ef3bba2622
Jun <jun.liu@omniata.com>
2014-11-04 12:48:33 +0200

    update the version

--- src/com/omniata/android/sdk/Omniata.java
+++ src/com/omniata/android/sdk/Omniata.java
@@ -19,7 +19,7 @@ public class Omniata {
 	
 	private static final String TAG       = "Omniata";
 	private static final String EVENT_LOG = "events";
-	private static final String SDK_VERSION = "android-1.1.2";
+	private static final String SDK_VERSION = "android-1.1.3";
 	
 	private static Omniata instance;
 	
